ddc004c08ff87aa8a9739f70d6adde7ea6138555
Kavindu Umayangana <161000554+kavinduUmayangana@users.noreply.github.com>
2024-07-03 16:22:56 +0530

    complete the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,8 +23,8 @@ const Header = () => {
   
 
   return (
-    <header className='fixed top-0 left-0 z-10 w-full shadow-md bg-blue-300
-'>
+    <header className='fixed top-0 left-0 z-10 w-full shadow-md bg-blue-300' 
+>
       <nav className='items-center justify-between py-4 md:flex md:px-10 px-7'>
         <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-950'>
           Kavindu Umayangana
@@ -33,13 +33,13 @@ const Header = () => {
           {open ? <CloseIcon /> : <DensityMediumIcon />}
         </div>
         <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-blue-300
- md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-15 opacity-100':'top-[-490px]'} md:opacity-100 opacity-0`}>
+ md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in 
+ ${open ? 'top-15 opacity-100':'top-[-490px]'} md:opacity-100 opacity-0`}>
           {Links.map((link) => (
-            <li 
-            key={link.name} 
-           
-              className='text-xl md:ml-8 md:my-0 my-7'>
-              <ScrollLink to={link.link} smooth={true} duration={500} className='duration-500 text-gray-950 hover:text-gray-700'
+            <li key={link.name} className='text-xl md:ml-8 md:my-0 my-7' >
+              <ScrollLink to={link.link}
+                          smooth={true}
+                          duration={500}
                onClick={()=> setOpen(false)}
               >
                 {link.name}
